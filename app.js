cd4e9a79a88d6e7b7c8dbdb071db594b6f6e26ee
chaukz <reboningochauke@gmail.com>
2026-06-28 04:42:26 +0200

    final YelpCamp - helmet, mongo atlas, session store

--- app.js
+++ app.js
@@ -1,36 +1,98 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
-
 const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const engine = require('ejs-mate');
-const joi = require('joi');
-const { campgroundSchema, reviewSchema } = require('./schemas');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
-
-const passport = require('passport'); 
+const passport = require('passport');
 const localStrategy = require('passport-local');
-
 const User = require('./models/user');
-
-const userRoutes = require('./routes/users'); 
+const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
- 
+const expressMongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const MongoStore = require('connect-mongo');
 
+const DBurl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdn.maptiler.com/",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdn.maptiler.com/",
+];
+const connectSrcUrls = [
+    "https://api.maptiler.com/",
+];
+const fontSrcUrls = [];
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Error connecting to MongoDB:', err));
+app.use(expressMongoSanitize({ replaceWith: '_' }));
+app.use(helmet());
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`,
+                "https://images.unsplash.com/",
+                "https://api.maptiler.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
+mongoose.connect(DBurl, {})
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Error connecting to MongoDB:', err));
 
+const store = MongoStore.create({
+    mongoUrl: DBurl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret: 'thisshouldbeabettersecret!'
+    }
+});
+
+store.on('error', function (e) {
+    console.log('SESSION STORE ERROR', e);
+});
+
+const sessionConfig = {
+    store,
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+}
 
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
@@ -38,87 +100,34 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-const sessionConfig = {
-  secret:'thisshouldbeabettersecret!',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    dexpires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-    maxAge: 1000 * 60 * 60 * 24 * 7,
-    httpOnly: true // cookie cannot be accessed by client-side JavaScript
-  }
-}
 app.use(session(sessionConfig));
 app.use(flash());
-
 app.use(passport.initialize());
 app.use(passport.session());
-app.use((req, res, next) => {
-  res.locals.currentUser = req.user;
-  next();
-});
-
 passport.use(new localStrategy(User.authenticate()));
-
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
 app.use((req, res, next) => {
-  res.locals.success = req.flash('success');
-  res.locals.error = req.flash('error');
-  next();
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
 });
-
-
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map(el => el.message).join(',');
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
-
-
-const validateCampground = (req, res, next) => {
-  const campgroundSchema = joi.object({
-    campground: joi.object({
-      title: joi.string().required(),
-      price: joi.number().required().min(0),
-      image: joi.string().required(),
-      location: joi.string().required(),
-      description: joi.string().required()
-    }).required()
-  })
-  const { error } = campgroundSchema.validate(req.body);
-  
-  if (error) {
-    const msg = error.details.map(el => el.message).join(',');
-    throw new ExpressError(msg, 400);
-  }else {
-    next();
-  } 
-};
 
 app.use('/', userRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes);
-
-
 app.get('/', (req, res) => res.render('home'));
 
 app.all('*path', (req, res, next) => {
-  next(new ExpressError('Page Not Found', 404));
+    next(new ExpressError('Page Not Found', 404));
 });
 
-app.use((err,req,res,next) => {
-  const { statusCode = 500 } = err;
-  if (!err.message)  err.message = 'Oh No, Something Went Wrong!';
-  res.status(statusCode).render('error', { err });
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!';
+    res.status(statusCode).render('error', { err });
 });
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000/'));
